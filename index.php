b3bc147db627dfabfbc5873bd489708e6a7d584d
RoldValiente <84085367+RoldValiente@users.noreply.github.com>
2022-04-13 21:15:25 +0800

    try sa xampp (#5)

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-LeniKiko2022
+<!--Student Name: Harold Clyde J. Valiente-->
+<!--Laboratory Exercise: #1-->
+<html>
+    <head>
+        <title>Me and PHP</title>
+    </head>
+    
+    <body>
+        <?php
+            // handson1: my first PHP program
+            print "<h1>This is my first PHP program.</h1>";
+            print "But <b>not</b> my last!";
+        ?>
+    </body>
+</html>
