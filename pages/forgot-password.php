134ff47aa3bc68f1141f89f4a54498a41305d9f0
RoldValiente <84085367+RoldValiente@users.noreply.github.com>
2022-04-15 21:26:56 +0800

    Gibutangan na nakog assets folder for the fonts and images

--- pages/forgot-password.php
+++ pages/forgot-password.php
@@ -8,30 +8,29 @@
     <body>
         <div id="box">
             <p id="back"><a href="log-in.php"><u>Back</u></a></p>
+            
             <div id="bar">
                 <div id="logo">
-                    <img src="../logo.png" alt="logo">
+                    <img src="../assets/images/prescription-logo.png" alt="logo">
                 </div>
             </div>
+
             <div id="content">
                 <b>Forgot your password?</b>
                 <br>
                 <p>No worries! Enter your email address below and we'll send you instructions on how to reset your password.</p>
             </div>
 
-            <form action="password-reset.php"> <!--Sends the inputs of the form to the current executing script-->
+            <form action="password-reset.php">
                 <div>
                     <input id= "inputted" type="text" placeholder="Email address *" required>
                 </div>
 
-                <input id = "button" value="Send Request" type="submit"><!--Submit Button-->
+                <input id = "button" value="Send Request" type="submit">
             </form>
             <br>
 
             <a href="password-reset.php"><button type="button">I have a confirmation code.</button></a>
-
-        </div>
-        
-        
+        </div>        
     </body>
 </html>
