fbb146370a23b4f5cb032b68bb1da0967aa97abb
Ike Gabriel Yuson <89395727+ikeman09@users.noreply.github.com>
2022-04-16 04:56:53 +0800

    feature/homepage (#11)
    * changed docker compose
    * edit docker image
    * added homepage

---
+++ pages/homepage.php
@@ -0,0 +1,93 @@
+<html>
+<head>
+  <title>DMPP</title>
+  <link rel="stylesheet" href="../css/homepage_styles/homepage.css">
+  <meta content="width=device-width, initial-scale=1" name="viewport" />
+</head>
+<body>
+<div class="homepage">
+  <section class="navbar">
+    <div class="navbar__wrapper">
+      <a href="homepage.php">
+        <img class="navbar__logo" alt="DMPPLogo" src="../assets/images/prescription-logo.png">
+      </a>
+      <a class="navbar__button" href="sign-up.php">Get Started</a>
+    </div>
+  </section>
+  <section class="dmpp">
+    <div class="dmpp__wrapper">
+      <div class="dmpp__left">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="dmpp__right">
+        <h1>Digital Medical Prescription</h1>
+      </div>
+    </div>
+  </section>
+
+  <section class="how-it-works">
+    <div class="how-it-works__items">
+      <div class="how-it-works__item">
+        <div class="how-it-works__item-image"></div>
+        <h1>Doctor</h1>
+      </div>
+      <div class="how-it-works__arrow"><img src="../assets/images/arrow.svg" alt="Arrow pointing to the right"></div>
+      <div class="how-it-works__item">
+        <div class="how-it-works__item-image"></div>
+        <h1>Patient</h1>
+      </div>
+      <div class="how-it-works__arrow"><img src="../assets/images/arrow.svg" alt="Arrow pointing to the right"></div>
+      <div class="how-it-works__item">
+        <div class="how-it-works__item-image"></div>
+        <h1>Pharmacy</h1>
+      </div>
+    </div>
+  </section>
+
+  <section class="doctor">
+    <div class="doctor__wrapper">
+      <div class="doctor__left">
+        <h1>Doctor</h1>
+      </div>
+      <div class="doctor__right">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="mainhero">
+    <div class="mainhero__wrapper">
+      <div class="mainhero__left">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+      <div class="mainhero__right">
+        <h1>Patient</h1>
+      </div>
+    </div>
+  </section>
+  <section class="pharmacy">
+    <div class="pharmacy__wrapper">
+      <div class="pharmacy__left">
+        <h1>Pharmacy</h1>
+      </div>
+      <div class="pharmacy__right">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="footer">
+    <div class="footer__wrapper">
+      <div class="footer__left">
+        <p>Contact Us</p>
+      </div>
+      <div class="footer__right">
+        <p><a href="log-in.php">Login</a></p>
+        <p><a href="sign-up.php">Sign up</a></p>
+        <p><a href="sign-up-pharmacy.php">Register your Institution</a></p>
+      </div>
+    </div>
+  </section>
+</div>
+</body>
+</html>
