539ef84f0d9df9c31b2d72d85889705e869fccce
Abby Gayle Pandaan <arpandaan@up.edu.ph>
2022-04-22 15:14:43 +0800

    history

---
+++ pages/pharmacy-history.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DMPP</title>
+        <link rel="stylesheet" href="../css/pharmacy-history.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    
+    <body>
+        <div id="box">
+            <div id="bar">
+                <div id="logo">
+                    <img src="../assets/images/prescription-logo.png" alt="logo">
+                    <a class="navbar__button" href="pharmacy-change-password.php">Change Password</a>
+                </div>
+            </div>
+            
+            <div class="purchase">
+              <p>Purchase #20200329-0001<br><br> 1 x Paracetamol 500mg - 1.00<br> 1 x Paracetamol 500mg - 1.00<br>1 x Paracetamol 500mg - 1.00<br><br>Total: 3.00</p>
+            </div>
+
+            <div class="purchase">
+              <p>Purchase #20200329-0001<br><br> 1 x Paracetamol 500mg - 1.00<br> 1 x Paracetamol 500mg - 1.00<br>1 x Paracetamol 500mg - 1.00<br><br>Total: 3.00</p>
+            </div>
+
+            <div class="purchase">
+              <p>Purchase #20200329-0001<br><br> 1 x Paracetamol 500mg - 1.00<br> 1 x Paracetamol 500mg - 1.00<br>1 x Paracetamol 500mg - 1.00<br><br>Total: 3.00</p>
+            </div>
+
+            <div class="navbar">
+                <a href="pharmacy-inventory.php">
+                  <div class="inventory">
+                    <img src="../assets/images/circle.png" alt="circle">
+                    <p>Inventory</p>
+                </a>
+                <a href="pharmacy-scan.php">
+                  <div class="scan">
+                    <img src="../assets/images/qr.png" alt="qr code">
+                    <p>Scan Prescriptions</p>
+                </a>
+                <div class="history">
+                  <img src="../assets/images/circle.png" alt="circle">
+                  <p>History</p>
+                </div>
+            </div>
+
+        </div>     
+    </body>
+</html>
