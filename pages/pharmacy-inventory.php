cb2ad9c75cecf32c7ddec8d8a1e0121cb705db48
Abby Gayle Pandaan <arpandaan@up.edu.ph>
2022-04-22 15:15:07 +0800

    inventory

---
+++ pages/pharmacy-inventory.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DMPP</title>
+        <link rel="stylesheet" href="../css/pharmacy-inventory.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    
+    <body>
+        <div id="box">
+            <div id="bar">
+                <div id="logo">
+                    <img src="../assets/images/prescription-logo.png" alt="logo">
+                    <a class="navbar__button" href="pharmacy-change-password.php">Change Password</a>
+                </div>
+            </div>
+            
+            <div class="qr">
+              <img src="../assets/images/paracetamol.jpg" alt="medicine">
+              <p>Paracetamol Tablets 500mg<br><br> 999 pieces left</p>            
+            </div>
+
+            <div class="qr">
+              <img src="../assets/images/paracetamol.jpg" alt="medicine">
+              <p>Paracetamol Tablets 500mg<br><br> 999 pieces left</p>            
+            </div>
+
+            <div class="qr">
+              <img src="../assets/images/paracetamol.jpg" alt="medicine">
+              <p>Paracetamol Tablets 500mg<br><br> 999 pieces left</p>            
+            </div>
+
+            <div class="qr">
+              <img src="../assets/images/paracetamol.jpg" alt="medicine">
+              <p>Paracetamol Tablets 500mg<br><br> 999 pieces left</p>            
+            </div>
+
+            <div class="navbar">
+                <div class="inventory">
+                  <img src="../assets/images/circle.png" alt="circle">
+                  <p>Inventory</p>
+                </div>
+                <a href="pharmacy-scan.php">
+                  <div class="scan">
+                    <img src="../assets/images/qr.png" alt="qr code">
+                    <p>Scan Prescriptions</p>
+                </a>
+                <a href="pharmacy-history.php">
+                  <div class="history">
+                    <img src="../assets/images/circle.png" alt="circle">
+                    <p>History</p>
+                </a>
+            </div>
+
+        </div>     
+    </body>
+</html>
