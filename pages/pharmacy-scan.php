612842ed4370d16bdbc588b9135bd363ab40f932
RoldValiente <84085367+RoldValiente@users.noreply.github.com>
2022-06-07 00:46:30 +0800

    Pharmacy QR Scanner and Inventory (#35)

--- pages/pharmacy-scan.php
+++ pages/pharmacy-scan.php
@@ -25,24 +25,28 @@
 
     //mysqli_close($conn);
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Scan Prescriptions</title>
-        <link rel="stylesheet" type="text/css" href="../css/pharmacy-scan.css">
+        <title>2</title>
+        <link rel="stylesheet" href="../css/pharmacy-scan.css">
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
         <script src="../javascript/instascan.min.js"></script>
     </head>
     
     <body>
-        <div id="bar">
-            <div id="logo">
+        <div id="bar" class="container">
+            <div id="logo" class="item">
                 <img src="../assets/images/prescription-logo.png" alt="logo">
-                <a class="navbar__button" href="pharmacy-change-password.php">Change Password</a>
+            </div>
+            <div id="changePass" class="item">
+                <a href="pharmacy-change-password.php">
+                    <input id="button1" value="Change Password" type="submit">
+                </a>
             </div>
         </div>
-        <div id="box">
+
+        <div id="box" class="container">
             <div id="qr">
                 <video id="preview" autoplay="autoplay"></video>
             </div>
@@ -57,11 +61,11 @@
                     <input id="inputted" type="text" name="qr" required>
                 </div>
 
-                <input id="button" value="Enter" type="submit">
+                <input id="button2" value="Enter" type="submit">
             </form>
 
-            <a href="pharmacy-scan.php">
-                <input id="button2" value="Scan Again" type="submit">
+            <a href="2.php">
+                <input id="button3" value="Scan Again" type="submit">
             </a>
 
             <?php
@@ -116,12 +120,12 @@
                             echo "
                             <script>
                                 document.getElementById(\"enter\").innerHTML = \"Prescription in the database!\";
-                                document.getElementById(\"button\").value = \"Scan Again\";
+                                document.getElementById(\"button2\").value = \"Scan Again\";
                                 document.getElementById(\"preview\").style.display = \"none\";
-                                document.getElementById(\"status\").style.visibility = \"visible\";
-                                document.getElementById(\"status\").style.width = \"70%\";
+                                document.getElementById(\"status\").style.display = \"block\";
+                                document.getElementById(\"status\").style.width = \"50%\";
                                 document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                                document.getElementById(\"button2\").style.visibility = \"visible\";
+                                document.getElementById(\"button3\").style.visibility = \"visible\";
                             </script>";
                         }
                         else
@@ -129,26 +133,26 @@
                             echo "
                             <script>
                                 document.getElementById(\"enter\").innerHTML = \"Pharmacy has insufficient stock!\";
-                                document.getElementById(\"button\").value = \"Scan Again\";
+                                document.getElementById(\"button2\").value = \"Scan Again\";
                                 document.getElementById(\"preview\").style.display = \"none\";
                                 document.getElementById(\"status\").src = \"../assets/images/qr_failure.png\";
-                                document.getElementById(\"status\").style.visibility = \"visible\";
-                                document.getElementById(\"status\").style.width = \"70%\";
+                                document.getElementById(\"status\").style.display = \"block\";
+                                document.getElementById(\"status\").style.width = \"50%\";
                                 document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                                document.getElementById(\"button2\").style.visibility = \"visible\";
+                                document.getElementById(\"button3\").style.visibility = \"visible\";
                             </script>";
                         }
                     }
                     else
                     {
                         echo "<script>
-                        document.getElementById(\"button\").value = \"Scan Again\";
+                        document.getElementById(\"button2\").value = \"Scan Again\";
                         document.getElementById(\"preview\").style.display = \"none\";
                         document.getElementById(\"status\").src = \"../assets/images/qr_failure.png\";
-                        document.getElementById(\"status\").style.visibility = \"visible\";
-                        document.getElementById(\"status\").style.width = \"70%\";
+                        document.getElementById(\"status\").style.display = \"block\";
+                        document.getElementById(\"status\").style.width = \"50%\";
                         document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                        document.getElementById(\"button2\").style.visibility = \"visible\";
+                        document.getElementById(\"button3\").style.visibility = \"visible\";
                         </script>";
 
                         if($state == true && $bought == true)
@@ -165,28 +169,28 @@
                     }
                 }
             ?>
+        </div>
 
-            <div class="navbar">
+        <div id="navbar" class="container">
+            <div id="other" class="item">
                 <a href="pharmacy-inventory.php">
-                    <div class="inventory">
-                        <img src="../assets/images/circle.png" alt="circle">
-                        <p>Inventory</p>
-                    </div>
+                    <img src="../assets/images/circle.png" alt="circle">
+                    <p>Inventory</p>
                 </a>
+            </div> 
 
-                <div class="scan">
-                    <img src="../assets/images/qrcode-small.png" alt="qr code">
-                    <p>Scan Prescriptions</p>
-                </div>
-
+            <div id="inventory" class="item">
+                <img src="../assets/images/qrcode-small.png" alt="qr code">
+                <p>Scan Prescriptions</p>
+            </div>
+            
+            <div id="other" class="item">
                 <a href="pharmacy-history.php">
-                    <div class="history">
-                        <img src="../assets/images/circle.png" alt="circle">
-                        <p>History</p>
-                    </div>
+                    <img src="../assets/images/circle.png" alt="circle">
+                    <p>History</p>
                 </a>
             </div>
-        </div>   
+        </div>
         <script src="../javascript/pharmacy-scan.js"></script> 
     </body>
 </html>
