3d893af3b1860a0be17540b4440534213f815170
John Servo <jaservo@up.edu.ph>
2022-06-06 22:07:04 +0800

    changed header link to patient-toclaim

--- pages/patient/patient-claimed.php
+++ pages/patient/patient-claimed.php
@@ -41,7 +41,7 @@
         </div>
 
         <div class="section">
-            <a href="./patient-toclaim-empty.php">
+            <a href="./patient-toclaim.php">
                 <p id="to-claim"> To Claim </p>
             </a>
             <a href="./patient-claimed.php">
