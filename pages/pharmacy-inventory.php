533e04505a4bffd35a55454542d84fa4a607f37a
RoldValiente <84085367+RoldValiente@users.noreply.github.com>
2022-06-06 16:58:24 +0800

    Updated dbh.sql (#29)

--- pages/pharmacy-inventory.php
+++ pages/pharmacy-inventory.php
@@ -21,7 +21,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>DMPP</title>
+        <title>Inventory</title>
         <link rel="stylesheet" href="../css/pharmacy-inventory.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
