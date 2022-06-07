8fa5fac968211e3edb83c2ee783104d011a627e6
RoldValiente <84085367+RoldValiente@users.noreply.github.com>
2022-06-07 21:24:56 +0800

    Update sa Log in and Pharmacy Scan and Inventory (#45)

--- pages/2.php
+++ pages/2.php
@@ -9,17 +9,18 @@
 
     $pid = $_SESSION['pharmaID'];
 
-    $sql = 'SELECT * FROM prescription INNER JOIN prescription_status ON prescription.prescriptionID = prescription_status.prescriptionID';
+    $sql = "SELECT * FROM history INNER JOIN prescription ON history.prescriptionID = prescription.prescriptionID 
+    WHERE history.pharmacyID = '".$pid."'";
 
     $result = mysqli_query($conn, $sql);
 
-    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $dig = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    $inventory = "SELECT medicineID, quantityLeft FROM inventory WHERE pharmacyID ='".$pid."'";
+    $sql = "SELECT pharmacyName from pharmacy_info WHERE pharmacyID = '".$pid."'";
 
-    $result2 = mysqli_query($conn, $inventory);
-    
-    $data2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+    $result = mysqli_query($conn, $sql);
+
+    $name = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     //mysqli_free_result($result);
 
@@ -28,10 +29,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>2</title>
+        <title>Inventory</title>
         <link rel="stylesheet" href="2.css">
-        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
-        <script src="../javascript/instascan.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     
     <body>
@@ -41,152 +41,71 @@
             </div>
             <div id="changePass" class="item">
                 <a href="pharmacy-change-password.php">
-                    <input id="button1" value="Change Password" type="submit">
+                    <input id="button" value="Change Password" type="submit">
                 </a>
             </div>
         </div>
 
+        <div id="header">
+            <?php 
+            foreach($name as $pharmaName)
+            {
+                print htmlspecialchars($pharmaName['pharmacyName'])." Inventory";
+            }?>
+        </div>
+
         <div id="box" class="container">
-            <div id="qr">
-                <video id="preview" autoplay="autoplay"></video>
-            </div>
+            <?php foreach($dig as $digi) {?>
+                <div class="medicine">
+                    <div id="image">
+                        <img src="../assets/images/meds.png" alt="medicine">
+                    </div>
+                    <div id="text">
+                        <?php
+                            $medID = $digi['medicineID'];
+                            $presID = $digi['prescriptionID'];
+                            $sql2 = "SELECT * FROM prescription_medicine WHERE prescription_medicine.id = '".$medID."'";
+                            $result2 = mysqli_query($conn, $sql2);
+                            $medName = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
-            <img id="status" src="../assets/images/qr_success.png">
-            <p id="demo"></p>
+                            $sql3 = "SELECT price FROM inventory WHERE inventory.pharmacyID = '".$pid."'AND inventory.medicineID = ".$medID."";
+                            $result3 = mysqli_query($conn, $sql3);
+                            $price = mysqli_fetch_all($result3, MYSQLI_ASSOC);
 
-            <p id="enter">Trouble scanning? Enter the code below: </p>
-
-            <form id="myForm" method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                <div>
-                    <input id="inputted" type="text" name="qr" required>
-                </div>
-
-                <input id="button2" value="Enter" type="submit">
-            </form>
-
-            <a href="2.php">
-                <input id="button3" value="Scan Again" type="submit">
-            </a>
-
-            <?php
-                if ($_SERVER["REQUEST_METHOD"]== "POST")
-                {
-                    $quantity = 0;
-                    $mid = "";
-                    $pcs = 0;
-                    $qr = $_REQUEST["qr"];
-                    $state = false;
-                    $bought = false;
-                    $sufficient = false;
-
-                    foreach($data as $prescription)
-                    {
-                        if($qr == $prescription['prescriptionID'])
-                        {
-                            $state = true;
-                            if($prescription['claimedStatus'] == 1)
+                            print "Prescription ID: " . htmlspecialchars($digi['prescriptionID']) . "<br>";
+                            foreach($medName as $name)
                             {
-                                $bought = true;
-                                break;
+                                print "Medicine Prescribed: ".htmlspecialchars($name['genericName_dosage'])." ".htmlspecialchars($name['brandName_dosage'])."<br>";
                             }
-                            $mid = $prescription['medicineID'];
-                            $pcs = $prescription['pieces'];
-                            break;
-                        }
-                    }
-            
-                    foreach($data2 as $medicine)
-                    {
-                        if($medicine['medicineID'] == $mid && $medicine['quantityLeft'] >= $pcs)
-                        {
-                            $sufficient = true;
-                            $quantity = $medicine['quantityLeft'] - $pcs;
-                            echo $quantity;
-                            break;
-                        }
-                    }
+                            print "Pieces Purchased: " . htmlspecialchars($digi['pieces']) . "<br>";
+                            foreach($price as $amount)
+                            {
+                                print "<div id=quantity> TOTAL: PHP ". $amount['price']*$digi['pieces']."</div>";
+                            }
+                        ?>
+                        <!--<p>pieces left</p>-->
 
-                    if($state == true && $bought == false)
-                    {
-                        if($sufficient == true)
-                        {
-                            $insert = "INSERT INTO history (pharmacyID, prescriptionID) VALUES ('$pid', '$qr')";
-                            mysqli_query($conn, $insert);
-
-                            $update = "UPDATE prescription_status, inventory SET prescription_status.claimedStatus = 1, inventory.quantityLeft = $quantity 
-                            WHERE prescription_status.prescriptionID = '$qr'AND (inventory.pharmacyID = '$pid' AND inventory.medicineID = '$mid')";
-                            mysqli_query($conn, $update);
-
-                            echo "
-                            <script>
-                                document.getElementById(\"enter\").innerHTML = \"Prescription in the database!\";
-                                document.getElementById(\"button2\").value = \"Scan Again\";
-                                document.getElementById(\"preview\").style.display = \"none\";
-                                document.getElementById(\"status\").style.display = \"block\";
-                                document.getElementById(\"status\").style.width = \"50%\";
-                                document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                                document.getElementById(\"button3\").style.visibility = \"visible\";
-                            </script>";
-                        }
-                        else
-                        {
-                            echo "
-                            <script>
-                                document.getElementById(\"enter\").innerHTML = \"Pharmacy has insufficient stock!\";
-                                document.getElementById(\"button2\").value = \"Scan Again\";
-                                document.getElementById(\"preview\").style.display = \"none\";
-                                document.getElementById(\"status\").src = \"../assets/images/qr_failure.png\";
-                                document.getElementById(\"status\").style.display = \"block\";
-                                document.getElementById(\"status\").style.width = \"50%\";
-                                document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                                document.getElementById(\"button3\").style.visibility = \"visible\";
-                            </script>";
-                        }
-                    }
-                    else
-                    {
-                        echo "<script>
-                        document.getElementById(\"button2\").value = \"Scan Again\";
-                        document.getElementById(\"preview\").style.display = \"none\";
-                        document.getElementById(\"status\").src = \"../assets/images/qr_failure.png\";
-                        document.getElementById(\"status\").style.display = \"block\";
-                        document.getElementById(\"status\").style.width = \"50%\";
-                        document.getElementById(\"myForm\").style.visibility = \"hidden\";
-                        document.getElementById(\"button3\").style.visibility = \"visible\";
-                        </script>";
-
-                        if($state == true && $bought == true)
-                        {
-                            echo "<script>
-                            document.getElementById(\"enter\").innerHTML = \"Prescription already claimed!\";</script>";
-                        }
-                        else
-                        {
-                            echo "<script>
-                            document.getElementById(\"enter\").innerHTML = \"Prescription not in the database!\";</script>";
-                        }
-
-                    }
-                }
-            ?>
+                    </div>
+                </div>
+            <?php }?>
         </div>
 
         <div id="navbar" class="container">
             <div id="other" class="item">
-                <a href="pharmacy-inventory.php">
-                    <img src="../assets/images/circle.png" alt="circle">
-                    <p>Inventory</p>
-                </a>
+                <img src="../assets/images/circle.png" alt="circle">
+                <p>Inventory</p>
             </div> 
 
-            <div id="inventory" class="item">
-                <img src="../assets/images/qrcode-small.png" alt="qr code">
-                <p>Scan Prescriptions</p>
+            <div id="other" class="item">
+                <a href="pharmacy-scan.php">
+                    <img src="../assets/images/qrcode-small.png" alt="qr code">
+                    <p>Scan Prescriptions</p>
+                </a>
             </div>
             
-            <div id="other" class="item">
+            <div id="history" class="item">
                 <a href="pharmacy-history.php">
-                    <img src="../assets/images/circle.png" alt="circle">
+                    <img src="../assets/images/Circle_(indigo).png" alt="circle">
                     <p>History</p>
                 </a>
             </div>
